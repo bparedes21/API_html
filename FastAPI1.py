1f7cac1592f83c7ecc86234bd01150eab2ce6dae
bparedes21 <gestionorganizacion17@gmail.com>
2023-11-20 18:24:16 -0300

    db5

--- FastAPI1.py
+++ FastAPI1.py
@@ -52,4 +52,4 @@ async def read_form(request: Request):
     rows = database.fetch_all(query=query)
     # Desconectar de la base de datos
     database.disconnect()
-    return templates.TemplateResponse({"request": rows})
+    return templates.TemplateResponse("show_data.html", {"request": request, "data": rows})
