43cc2ff57b8300bf96cf8d7044cd432da32f7f80
bparedes21 <gestionorganizacion17@gmail.com>
2023-11-20 17:16:24 -0300

    db1

--- FastAPI1.py
+++ FastAPI1.py
@@ -4,13 +4,9 @@ from databases import Database
 
 app = FastAPI()
 
-# Configuración de la base de datos SQLite
-DATABASE_URL = "sqlite:///./test.db"
-database = Database(DATABASE_URL)
 
 # Configuración de plantillas Jinja2
 templates = Jinja2Templates(directory="templates")
-
 # Ruta para el formulario
 @app.get("/")
 async def read_form(request: Request):
@@ -20,23 +16,6 @@ async def read_form(request: Request):
 @app.post("/submit/")
 async def submit_form(request: Request, title: str = Form(...), content: str = Form(...)):
     # Conexión a la base de datos
-    await database.connect()
 
-    # Crear una tabla si no existe
-    await database.execute(
-        """
-        CREATE TABLE IF NOT EXISTS notes (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            title TEXT,
-            content TEXT
-        )
-        """
-    )
-
-    # Insertar datos en la tabla
-    await database.execute("INSERT INTO notes (title, content) VALUES (:title, :content)", values={"title": title, "content": content})
-
-    # Desconectar de la base de datos
-    await database.disconnect()
 
     return templates.TemplateResponse("submitted.html", {"request": request, "title": title, "content": content})
