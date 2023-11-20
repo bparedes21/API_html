75537479a35653b6001f4d8818cd68f4de58c775
bparedes21 <gestionorganizacion17@gmail.com>
2023-11-20 16:28:07 -0300

    commit3

--- FastAPI1.py
+++ FastAPI1.py
@@ -1,44 +1,9 @@
-from fastapi import FastAPI, Form, Request
-from fastapi.templating import Jinja2Templates
-from databases import Database
+from fastapi import FastAPI
+
 
 app = FastAPI()
 
-# Configuración de la base de datos SQLite
-DATABASE_URL = "sqlite:///./test.db"
-database = Database(DATABASE_URL)
-
-# Configuración de plantillas Jinja2
-templates = Jinja2Templates(directory="templates")
-
 # Ruta para el formulario
 @app.get("/")
-async def read_form(request: Request):
-    return templates.TemplateResponse("form.html", {"request": request})
-"""
-# Ruta para procesar el formulario
-@app.post("/submit/")
-async def submit_form(request: Request, title: str = Form(...), content: str = Form(...)):
-    # Conexión a la base de datos
-    await database.connect()
-
-    # Crear una tabla si no existe
-    await database.execute(
-        
-   #     CREATE TABLE IF NOT EXISTS notes (
-   #        id INTEGER PRIMARY KEY AUTOINCREMENT,
-   #         title TEXT,
-   #         content TEXT
-   #     )
-        
-   
-    )
-
-    # Insertar datos en la tabla
-    await database.execute("INSERT INTO notes (title, content) VALUES (:title, :content)", values={"title": title, "content": content})
-
-    # Desconectar de la base de datos
-    await database.disconnect()
-
-    return templates.TemplateResponse("submitted.html", {"request": request, "title": title, "content": content})
-    """
+async def read_form():
+    return {"form.html", ""}
